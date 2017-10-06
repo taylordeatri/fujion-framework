e72fcd912e201177cea4a51813e3f047b2a693be
Douglas Martin <dkmartin@regenstrief.org>
2017-10-05 20:52:03 -0400

    Archetype updates.

--- fujion-archetype-parent/fujion-archetype-component/src/main/resources/archetype-resources/src/main/java/__ClassName__.java
+++ fujion-archetype-parent/fujion-archetype-component/src/main/resources/archetype-resources/src/main/java/__ClassName__.java
@@ -8,19 +8,19 @@ import org.apache.commons.logging.LogFactory;
 import org.fujion.annotation.Component;
 import org.fujion.annotation.Component.PropertyGetter;
 import org.fujion.annotation.Component.PropertySetter;
-import org.fujion.component.BaseComponent;
+import org.fujion.component.BaseUIComponent;
 
 @Component(tag = "${classname}", widgetModule = "${artifactId}", widgetClass = "${ClassName}", parentTag = "*")
-public class ${ClassName}Component extends BaseComponent {
+public class ${ClassName} extends BaseUIComponent {
 
-	private static final Log log = LogFactory.getLog(${ClassName}Component.class);
+	private static final Log log = LogFactory.getLog(${ClassName}.class);
 
-	public ${ClassName}Component() {
+	public ${ClassName}() {
 	}
 	
     @Override
     protected void _initProps(Map<String, Object> props) {
-        props.put("wclazz", "${artifactId}");
+        props.put("wclazz", "${classname}");
         super._initProps(props);
     }
 
