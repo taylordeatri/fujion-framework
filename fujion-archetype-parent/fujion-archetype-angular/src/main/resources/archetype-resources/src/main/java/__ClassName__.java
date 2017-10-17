9a6057abf686d258fbbeb0694cb6497229d522a7
Douglas Martin <dkmartin@regenstrief.org>
2017-10-17 03:36:05 -0400

    Add Angular Maven archetype

---
+++ fujion-archetype-parent/fujion-archetype-angular/src/main/resources/archetype-resources/src/main/java/__ClassName__.java
@@ -0,0 +1,33 @@
+package ${package};
+
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.fujion.annotation.Component;
+import org.fujion.annotation.Component.PropertyGetter;
+import org.fujion.annotation.Component.PropertySetter;
+import org.fujion.angular.AngularComponent;
+
+@Component(tag = "${classname}", widgetModule = "fujion-angular-widget", widgetClass = "AngularWidget", parentTag = "*")
+public class ${ClassName} extends AngularComponent {
+
+	private static final Log log = LogFactory.getLog(${ClassName}.class);
+
+	public ${ClassName}() {
+		super.setSrc("${artifactId}");
+	}
+	
+	@Override
+   protected void initProps(Map<String, Object> props) {
+   	super.initProps(props);
+   	props.put("wclazz", "${classname}");
+   }
+
+   @PropertySetter(value = "src", hide = true)
+   public void setSrc(String src) {
+      throw new UnsupportedOperationException();
+   }
+
+}
